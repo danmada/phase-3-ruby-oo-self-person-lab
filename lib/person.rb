def9464b4044a0e90e16fd3f49d0b937a6db7430
danmada <danmada@gmail.com>
2021-07-22 10:57:54 -0500

    commit tree 517e934f7d6c1aad5d8c4aa3ce936736410636da

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,31 @@
-# your code goes here
+class Person
+  attr_reader :name
+  attr_accessor :bank_account
+  def initialize (name)
+    @name = name
+    @happiness = 8
+    @hygiene = 8
+    @bank_account = 25
+  end
+
+  def happiness=(number)
+    if number <= 10
+      @happiness = number
+    end
+  end
+
+  def happiness
+    @happiness
+  end
+
+  def hygiene=(number)
+    if number <= 10
+      @hygiene = number
+    end
+  end
+
+  def hygiene
+    @hygiene
+  end
+
+end
